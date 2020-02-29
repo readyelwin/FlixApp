ddc9f29876842052caf229e0d6c1b8d470995ea6
readyelwin <readyelwin@gmail.com>
2020-02-28 19:22:45 -0500

    Add files via upload

---
+++ FlixApp/FlixApp/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  FlixApp
+//
+//  Created by Elwin Ready, Jr. on 2/28/20.
+//  Copyright © 2020 Elwin Ready, Jr. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
